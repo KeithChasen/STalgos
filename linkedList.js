044b4ffd152c401938daab177c2e14ac661e5742
keithchasen <keithchasen89@gmail.com>
2024-09-23 22:02:17 +0200

    linked list push

--- linkedList.js
+++ linkedList.js
@@ -7,7 +7,16 @@ class LinkedList {
     }
 
     push(value) {
-
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length ++;
+        return this;
     }
 
     unshift(value) {
@@ -27,5 +36,9 @@ class Node {
 }
 
 const myLinkedList = new LinkedList(4);
+
+myLinkedList.push(7)
+
+myLinkedList.push(10)
 
 console.log(myLinkedList)
